59b5f2027b733f7cdd3759822bcb199c5e1a3a89
Eric Beanland <eric.beanland@gmail.com>
2017-01-02 18:36:33 -0600

    added print_short_summary() as default
    print_full_summary is now seperation option --summary

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod error;
 mod timeclock;
 mod util;
 
+use chrono::*;
 use error::WorklogError;
 use getopts::Options;
 use std::env;
@@ -25,6 +26,9 @@ static CSV_FILE_NAME: &'static str = ".worklog.csv";
 #[cfg(target_family = "windows")]
 static CSV_FILE_NAME: &'static str = "worklog.csv";
 
+// TODO This belongs in a config file somewhere
+const WEEKSTART: i64 = Weekday::Sat as i64;
+
 
 fn print_full_summary(file: &File) -> Result<u8, WorklogError> {
     let csv_entries = try!(timeclock::read_timesheet(file));
@@ -39,6 +43,26 @@ fn print_full_summary(file: &File) -> Result<u8, WorklogError> {
 
     println!("Total Hours: {:.2}", total_hours);
     Ok(0)
+}
+
+
+fn print_short_summary(file: &File,
+                       since: Date<FixedOffset>)
+                       -> Result<(), WorklogError> {
+    let csv_entries = try!(timeclock::read_timesheet(file));
+    let entries = timeclock::pair_time_entries(csv_entries);
+    let records = timeclock::collect_date_records(entries);
+
+    let mut total_hours: f64 = 0.0;
+    for rec in records {
+        if rec.date > since {
+            total_hours += rec.hours();
+            println!("{}", rec);
+        }
+    }
+
+    println!("Total Hours: {:.2}", total_hours);
+    Ok(())
 }
 
 
@@ -70,6 +94,7 @@ fn main0() -> Result<i8, WorklogError> {
     opts.optflagopt("i", "in", "in time", "TIME");
     opts.optflagopt("o", "out", "out time", "TIME");
     opts.optopt("m", "memo", "the memo", "MEMO");
+    opts.optflag("s", "summary", "show the full summary");
     opts.optflag("h", "help", "print this help menu");
 
     let matches = try!(opts.parse(&args[1..]));
@@ -109,8 +134,13 @@ fn main0() -> Result<i8, WorklogError> {
         let dir_str = format!("{}", dir).to_lowercase();
         println!("Clocked {} at {}", dir_str, time.format("%F %I:%M %P"));
 
-    } else {
+    } else if matches.opt_present("s") {
         try!(print_full_summary(&csv_file));
+
+    } else {
+        let days_back = (-WEEKSTART + 7) % 7;
+        let start_date = util::now().date() - Duration::days(days_back);
+        try!(print_short_summary(&csv_file, start_date));
     }
 
     Ok(0)
