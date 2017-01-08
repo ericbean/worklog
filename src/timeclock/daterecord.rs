3b757906f4a8344be49527232549050e87c00021
Eric Beanland <eric.beanland@gmail.com>
2017-01-07 21:40:43 -0600

    unittests for DateRecord

--- src/timeclock/daterecord.rs
+++ src/timeclock/daterecord.rs
@@ -25,6 +25,18 @@ impl DateRecord {
         return dr;
     }
 
+
+    #[allow(dead_code)]
+    /// Construct a DateRecord from it's constituent parts
+    pub fn from_parts(date: Date<FixedOffset>, dur: f64, memo: &str) -> Self {
+        DateRecord {
+            date_: date,
+            duration: dur,
+            memo_: memo.to_owned(),
+        }
+    }
+
+
     /// Get the duration, expressed in seconds
     pub fn seconds(&self) -> f64 {
         self.duration
@@ -75,5 +87,42 @@ impl DateRecord {
 impl fmt::Display for DateRecord {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} {:.2} {}", self.date(), self.hours(), self.memo())
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use chrono::*;
+    use super::*;
+
+    fn daterecord_helper() -> DateRecord {
+        let x = NaiveDate::parse_from_str("2017-01-07", "%F").unwrap();
+        let y = FixedOffset::west(6 * 3600).from_local_date(&x).unwrap();
+        DateRecord::from_parts(y, 4321.098765, "Test")
+    }
+
+    #[test]
+    fn duration_methods_test() {
+        let mut dr = daterecord_helper();
+        // check math
+        assert!(dr.seconds() == 4321.098765);
+        assert!(dr.minutes() == 72.01831274999999);
+        assert!(dr.hours() == 1.2003052125);
+        // check that addition works
+        dr.add_seconds(1357.246);
+        assert!(dr.seconds() == 5678.344765);
+    }
+
+    #[test]
+    fn memo_methods_test() {
+        let mut dr = daterecord_helper();
+        assert!(dr.memo() == "Test");
+        // append empty str
+        dr.append_memo("");
+        assert!(dr.memo() == "Test");
+        // append actual str
+        dr.append_memo("success!");
+        assert!(dr.memo() == "Test, success!");
     }
 }
